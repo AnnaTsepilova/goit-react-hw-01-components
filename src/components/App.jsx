fa9fa4d5830f38d05f0710bc35131e8a07c33dc9
Anna Tsepilova <virgo.tsav@gmail.com>
2022-12-27 13:51:10 +0200

    minor fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import Statistics from 'components/Statistics/Statistics';
 import FriendList from 'components/FriendList/FriendList';
 import user from 'data/user.json';
 import data from 'data/data.json';
-import tableData from 'data/transactions.json';
+// import tableData from 'data/transactions.json';
 import friends from 'data/friends.json';
 import Section from 'components/Section/Section';
 
